5ff2b9eef4805a2474af232d78c3b2ce56fecf83
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-12 16:43:59 +0900

    export common components

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,3 +1,6 @@
 export { default as Button } from "./atoms/button/button";
 export { default as Avatar } from "./atoms/avatar/avatar";
 export { default as Badge } from "./atoms/badge/badge";
+export { default as TabItem } from "./atoms/tab/tab-item";
+export { default as Tabs } from "./molecules/tab/tabs";
+export type { TabsItemProps } from "./molecules/tab/tabs";
