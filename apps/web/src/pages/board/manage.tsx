70beb9c0da99a48a49266ce11fab366384986876
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-12 16:44:11 +0900

    create default pages for project

---
+++ apps/web/src/pages/board/manage.tsx
@@ -0,0 +1,11 @@
+import ManageTabs from "../../components/board/manage/manage-tabs";
+const Manage = () => {
+  return (
+    <div>
+      <ManageTabs />
+      <div className="mt-6"></div>
+    </div>
+  );
+};
+
+export default Manage;
