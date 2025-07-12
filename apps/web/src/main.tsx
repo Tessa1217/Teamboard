ba5ac928b3d43607ac5df75eada01fc81e87a4e8
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-12 16:45:20 +0900

    apply router for main.tsx file

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,34 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes";
 import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Avatar, Button, Badge } from "@repo/ui";
-const App = () => (
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Button color="blue" disabled={true}>
-      button
-    </Button>
-    <Button color="red">button</Button>
-    <Button color="purple">button</Button>
-    <Badge>badge</Badge>
-    <Badge color="blue" shape="flat">
-      badge
-    </Badge>
-    <Badge color="red" onClose={() => console.log("closed")}>
-      closable badge
-    </Badge>
-    <Avatar altText="권유진" />
-    <Avatar altText="YuJin Kwon" />
-  </div>
-);
+
+const App = () => <RouterProvider router={router}></RouterProvider>;
 
 createRoot(document.getElementById("app")!).render(<App />);
