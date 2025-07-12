33d6c50191d11338b5f4a3e885c7e77076d1dc6a
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-12 16:45:03 +0900

    update project setting

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {},
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  // resolve: {
+  //   alias: {
+  //     "@repo/ui": path.resolve(__dirname, "../../packages/ui/*"),
+  //   },
+  // },
 });
